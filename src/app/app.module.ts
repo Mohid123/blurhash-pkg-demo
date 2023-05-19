df3f95395b00db813d8185d02cbffdc4c53db334
Mohid123 <muhammadmohid141@gmail.com>
2023-05-19 15:19:10 +0500

    blurhash demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgxBlurhashModule } from 'ngx-blurhash-render';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxBlurhashModule
   ],
   providers: [],
   bootstrap: [AppComponent]
